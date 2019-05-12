fcae43aa77834b4a323216fe48963814da22cad8
Muhammad Qasim <Qasim9872@gmail.com>
2019-05-12 23:34:17 +0100

    updated templates and added header and footer components

---
+++ components/WebsiteFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-footer dark height="auto">
+    <v-card class="flex" flat tile>
+      <v-card-title class="teal">
+        <strong class="subheading">{{ $t('footer.subtitle') }}</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="(icon, index) in icons"
+          :key="index"
+          class="mx-3"
+          dark
+          icon
+        >
+          <font-awesome-icon size="lg" :icon="icon" />
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2019 —
+        <strong>Ghazala Charitable Trust</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      icons: [
+        ['fab', 'facebook'],
+        ['fab', 'twitter'],
+        ['fab', 'google-plus'],
+        ['fab', 'linkedin'],
+        ['fab', 'instagram']
+      ]
+    }
+  }
+}
+</script>
---
+++ components/WebsiteNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-toolbar
+    absolute
+    color="teal lighten-3"
+    dark
+    scroll-off-screen
+    scroll-target="#scrolling-techniques"
+  >
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title :to="home">{{ $t('title') }}</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>favorite</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  computed: {
+    home() {
+      // eslint-disable-next-line no-console
+      console.log(this.localePath('index'))
+      return this.localePath('index')
+    }
+  }
+}
+</script>
