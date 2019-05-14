43c56a6e93e07604a6546659ae34e2e0c2043f44
Muhammad Qasim <Qasim9872@gmail.com>
2019-05-14 08:27:03 +0100

    updated the navbar to be reactive across screens

--- components/WebsiteNavbar.vue
+++ components/WebsiteNavbar.vue
@@ -1,36 +1,93 @@
 <template>
-  <v-toolbar fixed color="transparent" dark app>
-    <v-toolbar-title class="black--text showPointer" :to="home">{{
-      $t('title')
-    }}</v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
-    <v-toolbar-items class="hidden-sm-and-up">
-      <v-btn class="black--text" :to="about" flat>{{
-        $t('navbar.about')
-      }}</v-btn>
-      <v-btn class="black--text" flat>Link Two</v-btn>
-      <v-btn class="black--text" flat>Link Three</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+  <div class="nav">
+    <v-container fill-height class="px-0 py-2">
+      <v-layout column wrap align-center justify-start>
+        <v-btn
+          v-for="link in links"
+          :key="link.name"
+          :to="link.path"
+          class="nav-link pa-1 mx-0 my-4"
+          active-class="nav-link-active"
+          flat
+          depressed
+          nuxt
+        >
+          <font-awesome-icon rotation="90" class="nav-text" :icon="link.icon" />
+          &nbsp;
+          <span class="nav-text">
+            {{ link.name }}
+          </span>
+        </v-btn>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    home() {
-      return this.localePath('index')
-    },
-    about() {
-      return this.localePath('about')
+  data() {
+    return {
+      links: [
+        {
+          name: 'Home',
+          path: this.friendlyLink('index'),
+          icon: 'home'
+        },
+        {
+          name: 'About',
+          path: this.friendlyLink('about'),
+          icon: 'info'
+        }
+      ]
+    }
+  },
+  methods: {
+    friendlyLink(page) {
+      return this.localePath(page)
     }
   }
 }
 </script>
 
 <style>
-.showPointer {
-  cursor: pointer;
+.nav {
+  /* background-color: $shades.abyss */
+  background: -moz-linear-gradient(bottom, #ef2424 0%, #ffe000 100%);
+  /* FF3.6-15 */
+  background: -webkit-linear-gradient(bottom, #ef2424 0%, #ffe000 100%);
+  /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to bottom, #ef2424 0%, #ffe000 100%);
+
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  z-index: 3000;
+  width: 6%;
+  max-width: 6vh;
+}
+
+.nav-link {
+  -ms-transform: rotate(180deg);
+  -webkit-transform: rotate(180deg);
+  transform: rotate(180deg);
+
+  z-index: 6000;
+  writing-mode: vertical-rl;
+  min-width: 10px !important;
+  height: auto !important;
+}
+
+.nav-text {
+  font-size: 20px;
+  /* margin-left: 17px; */
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 0.03em;
+}
+
+.nav-link-active {
+  /* color: black; */
+  color: white !important;
+  transition: 0.2s color ease;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,51 @@
 <template>
   <v-app>
-    <!-- <website-nav-bar /> -->
-    <navigation />
-
-    <v-content>
-      <nuxt />
-    </v-content>
+    <div class="website-container">
+      <website-nav-bar />
+      <v-content class="website-content">
+        <nuxt />
+      </v-content>
+    </div>
     <website-footer />
   </v-app>
 </template>
 
 <script>
-// import WebsiteNavBar from '@/components/WebsiteNavBar'
-import Navigation from '@/components/Navigation'
+import WebsiteNavBar from '@/components/WebsiteNavBar'
+// import Navigation from '@/components/Navigation'
 import WebsiteFooter from '@/components/WebsiteFooter'
 
 export default {
   components: {
-    // WebsiteNavBar,
-    Navigation,
+    WebsiteNavBar,
+    // Navigation,
     WebsiteFooter
   }
 }
 </script>
 
 <style>
-html {
-  padding-bottom: 0;
+.website-container {
+  min-height: 100vh;
+  position: relative;
+}
+
+.website-content {
+  min-height: 100vh;
+  width: 94%;
+  margin-left: 6%;
+}
+
+@media screen and (max-width: 800px) {
+  .nav {
+    width: 12%;
+  }
+  .website-content {
+    margin-left: 12%;
+    width: 88%;
+  }
+  .nav-text {
+    font-size: 10px !important;
+  }
 }
 </style>
