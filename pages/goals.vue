f2884e1d98e15e2d5ace8373271702577adba5f5
Muhammad Qasim <Qasim9872@gmail.com>
2019-05-15 07:47:32 +0100

    Added additional pages

---
+++ pages/goals.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center row>
+      <v-flex xs3 class="primary--text display-2 text-xs-center">Goals</v-flex>
+    </v-layout>
+  </v-container>
+</template>
